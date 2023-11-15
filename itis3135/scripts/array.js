6808e4969e6ed297e3257ca3a8cfad93c2dfe35d
Mkolibas <143038732+Mkolibas@users.noreply.github.com>
2023-11-14 19:43:38 -0500

    f

--- itis3135/scripts/array.js
+++ itis3135/scripts/array.js
@@ -2,29 +2,41 @@ let persons = [];
 let salaries = [];
 
 function addSalary() {
-  const person = document.getElementById("personInput").value.trim();
-  const salary = document.getElementById("salaryInput").value.trim();
+    const person = document.getElementById("personInput").value.trim();
+    const salary = document.getElementById("salaryInput").value.trim();
 
-  if (person === '' || isNaN(Number(salary))) {
-    alert("Please enter valid name and numeric salary.");
-    return;
-  }
+    if (person === '' || isNaN(Number(salary))) {
+        alert("Please enter valid name and numeric salary.");
+        return;
+    }
 
-  persons.push(person);
-  salaries.push(Number(salary));
+    persons.push(person);
+    salaries.push(Number(salary));
 
-  document.getElementById("personInput").value = ''; // Clear input fields
-  document.getElementById("salaryInput").value = '';
+    document.getElementById("personInput").value = ''; // Clear input fields
+    document.getElementById("salaryInput").value = '';
 
-  moveCursorToNameField();
+    moveCursorToNameField();
+}
+
+function addRandomEmployee() {
+    const randomNames = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry"];
+    const randomSalaries = Math.floor(Math.random() * 50000) + 30000; // Random salary between $30,000 and $80,000
+
+    const randomName = randomNames[Math.floor(Math.random() * randomNames.length)];
+
+    persons.push(randomName);
+    salaries.push(randomSalaries);
+
+    displaySalary();
 }
 
 function displayResults() {
-  const avgSalary = calculateAverage(salaries);
-  const maxSalary = Math.max(...salaries);
+    const avgSalary = calculateAverage(salaries);
+    const maxSalary = Math.max(...salaries);
 
-  const resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = `
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = `
     <h2>Results</h2>
     <p>Average Salary: ${avgSalary}</p>
     <p>Highest Salary: ${maxSalary}</p>
@@ -32,23 +44,20 @@ function displayResults() {
 }
 
 function displaySalary() {
-  const tableBody = document.getElementById("table_body");
-  tableBody.innerHTML = '';
+    const tableBody = document.getElementById("table_body");
+    tableBody.innerHTML = '';
 
-  for (let i = 0; i < persons.length; i++) {
-    const row = `<tr><td>${persons[i]}</td><td>${salaries[i]}</td></tr>`;
-    tableBody.insertAdjacentHTML('beforeend', row);
-  }
+    for (let i = 0; i < persons.length; i++) {
+        const row = `<tr><td>${persons[i]}</td><td>${salaries[i]}</td></tr>`;
+        tableBody.insertAdjacentHTML('beforeend', row);
+    }
 }
 
 function calculateAverage(arr) {
-  const total = arr.reduce((acc, curr) => acc + curr, 0);
-  return (total / arr.length).toFixed(2);
+    const total = arr.reduce((acc, curr) => acc + curr, 0);
+    return (total / arr.length).toFixed(2);
 }
 
 function moveCursorToNameField() {
-  document.getElementById("personInput").focus();
+    document.getElementById("personInput").focus();
 }
-
-// Set cursor to name field on page load
-moveCursorToNameField();
